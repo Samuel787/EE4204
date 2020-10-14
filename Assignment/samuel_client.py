d2adda988630f82c43b4f0847fe41870b6d3e6e8
Samuel787 <suther.david.samuel@gmail.com>
2020-10-15 00:04:16 +0800

    wrote client and server in python

--- Assignment/samuel_client.py
+++ Assignment/samuel_client.py
@@ -1,27 +1,71 @@
 import socket
 import time
 import os
+import random
+from string import ascii_letters
 
 PORT = [5000, 5001]
 HOST = "127.0.0.1"
 FILE_TO_SENT = "sample.txt"
 POSITIVE_ACK = "pack".encode("utf-8")
 NEGATIVE_ACK = "nack".encode("utf-8")
+ACK_SIZE = 4
+PACKET_SIZE = 256
+CRC = "101101001000111"
+CORRUPT_INTENSITY = 0.25
+ERROR_PROBABILITY = 0.05
+
+def xor(a, b): 
+    result = [] 
+    for i in range(1, len(b)): 
+        if a[i] == b[i]: 
+            result.append('0') 
+        else: 
+            result.append('1') 
+    return ''.join(result) 
+
+def mod2div(divident, divisor): 
+    pick = len(divisor) 
+    tmp = divident[0 : pick]  
+    while pick < len(divident): 
+        if tmp[0] == '1': 
+            tmp = xor(divisor, tmp) + divident[pick] 
+        else: 
+            tmp = xor('0'*pick, tmp) + divident[pick] 
+        pick += 1
+    if tmp[0] == '1': 
+        tmp = xor(divisor, tmp) 
+    else: 
+        tmp = xor('0'*pick, tmp) 
+    checkword = tmp 
+    return checkword
+
+def encodeData(data, key): 
+    l_key = len(key)  
+    appended_data = data + '0'*(l_key-1) 
+    remainder = mod2div(appended_data, key) 
+    codeword = data + remainder 
+    return codeword
 
 def SendFile():
-    s = socket.socket()
+    global_counter = 0
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setblocking(1)
     try:
         s.connect((HOST, PORT[0]))
     except Exception as e:
         print(e)
         s.connect((HOST, PORT[1]))
 
-    filesize = str(os.path.getsize(FILE_TO_SENT))
-    filesize = filesize.encode("utf-8")
     file_size_correctly_sent = False
     while not file_size_correctly_sent:
+        filesize = str(os.path.getsize(FILE_TO_SENT))
+        filesize = encodeData(filesize, CRC)
+        print(f"Sending file size msg: {filesize}")
+        filesize = pre_process_before_send(filesize, CORRUPT_INTENSITY, ERROR_PROBABILITY)
+        filesize = filesize.encode("utf-8")
         s.send(filesize)
-        ack = s.recv(4)
+        ack = s.recv(ACK_SIZE)
         if ack == POSITIVE_ACK:
             print("Received Positive Ack")
             file_size_correctly_sent = True
@@ -30,30 +74,65 @@ def SendFile():
             file_size_correctly_sent = False
         else:
             print("Should never enter this state")
-
+        global_counter += 1
+        print(str(global_counter))
 
     with open(FILE_TO_SENT, "rb") as f:
-        bytesToSend = f.read(1024)
+        bytesToSend = "dummy".encode("utf-8")
         while bytesToSend.decode("utf-8") != "":
+            bytesToSend = f.read(PACKET_SIZE - len(CRC) + 1)
             msg_correctly_sent = False
             while not msg_correctly_sent:
                 print("Attempting to send data")
+                bytesToSend = encodeData(bytesToSend.decode("utf-8"), CRC)
+                bytesToSend = (pre_process_before_send(bytesToSend, CORRUPT_INTENSITY, ERROR_PROBABILITY).encode("utf-8"))
                 s.send(bytesToSend)
-                bytesToSend = f.read(1024)
-                ACK = s.recv(4)
-                if ACK == POSITIVE_ACK:
-                    print("Received Positive Ack")
-                    msg_correctly_sent = True
-                elif ACK == NEGATIVE_ACK:
-                    print("Received Negative Ack")
-                    msg_correctly_sent = False
-                else:
-                    print("Should never enter this state")
-
+                print(f"Sent: {bytesToSend.decode('utf-8')}")
+                # bytesToSend = f.read(PACKET_SIZE - len(CRC) + 1)
+                while True:
+                    ACK = s.recv(ACK_SIZE)
+                    if ACK:
+                        if ACK == POSITIVE_ACK:
+                            print("Received Positive Ack")
+                            msg_correctly_sent = True
+                            break
+                        elif ACK == NEGATIVE_ACK:
+                            print("Received Negative Ack")
+                            msg_correctly_sent = False
+                            break
+                        else:
+                            print("Should never enter this state")
+                            continue
+                global_counter += 1
+                print(str(global_counter))
+                print("\n")
+                time.sleep(1)
     print("File successfully transferred")
 
+def corrupt_data(message, corrupt_intensity):
+    corrupt_intensity = 1 - corrupt_intensity
+    new_message = ""
+    for i in range(len(message)):
+        val = random.random()
+        if val >= corrupt_intensity:
+            #print("Corrupting data")
+            new_message += random.choice(ascii_letters)
+        else:
+            new_message += message[i]
+    return new_message
+
+def pre_process_before_send(message, corrupt_intensity, error_probability):
+    val = random.random()
+    if val <= error_probability:
+        message = corrupt_data(message, corrupt_intensity)
+    return message
 
 def Main():
+    if(CORRUPT_INTENSITY > 1 or CORRUPT_INTENSITY < 0):
+        print("CORRUPT_INTENSITY Should be between 0 and 1")
+    if(ERROR_PROBABILITY > 1 or ERROR_PROBABILITY < 0):
+        print("ERROR PROBABILITY shoudl be between 0 and 1")
+    random.seed(time.time())
     SendFile()
 
 if __name__ == "__main__":
--- Assignment/samuel_server.py
+++ Assignment/samuel_server.py
@@ -7,16 +7,59 @@ POSITIVE_ACK = "pack".encode("utf-8")
 NEGATIVE_ACK = "nack".encode("utf-8")
 PORT = [5000, 5001]
 HOST = "127.0.0.1"
+ACK_SIZE = 4
+PACKET_SIZE = 1024
+CRC = "101101001000111"
+
+def xor(a, b): 
+    result = [] 
+    for i in range(1, len(b)): 
+        if a[i] == b[i]: 
+            result.append('0') 
+        else: 
+            result.append('1') 
+    return ''.join(result) 
+
+def mod2div(divident, divisor): 
+    pick = len(divisor)  
+    tmp = divident[0 : pick] 
+    while pick < len(divident): 
+        if tmp[0] == '1': 
+            tmp = xor(divisor, tmp) + divident[pick] 
+        else:
+            tmp = xor('0'*pick, tmp) + divident[pick] 
+        pick += 1
+    if tmp[0] == '1': 
+        tmp = xor(divisor, tmp) 
+    else: 
+        tmp = xor('0'*pick, tmp) 
+    checkword = tmp 
+    return checkword 
+   
+def decodeData(data, key): 
+    l_key = len(key) 
+    appended_data = data + '0'*(l_key-1) 
+    remainder = mod2div(appended_data, key) 
+    return remainder 
 
 def RecvFile(sock):
+    global_counter = 0
+    start_time = time.time()
     file_size_correctly_received = False
     while not file_size_correctly_received:
         file_size = sock.recv(1024)
         try:
-            file_size = int(file_size.decode("utf-8"))
+            file_size = file_size.decode("utf-8")
+            print(f"Received file size {file_size}")
+            remainder = decodeData(file_size, CRC)
+            if int(remainder) != 0:
+                raise Exception("file size received wrongly")
+            file_size = file_size[:-(len(CRC) - 1)]
+            file_size = int(file_size)
             if file_size < 0:
                 raise Exception("file size cannot be negative")
             file_size_correctly_received = True
+            print(f"THE FILE SIZE IS {file_size}")
             sock.send(POSITIVE_ACK)
             print("Sent positive ack for file size")
         except Exception as e:
@@ -24,28 +67,52 @@ def RecvFile(sock):
             print(e)
             sock.send(NEGATIVE_ACK)
             print("Sent negative ack for file size")
+        global_counter += 1
+        print(str(global_counter))
 
     received_size = 0
-    f = open("Received_file", "wb")
+    f = open("Received_file.txt", "wb")
     while received_size < file_size:
         msg_correctly_received = False
         while not msg_correctly_received:
-            data = sock.recv(1024)
+            data = sock.recv(PACKET_SIZE)
             # do some checking of the message
-            msg_correctly_received = True
-            if msg_correctly_received:
-                received_size += len(data)
-                f.write(data)
-                print("Sending Positive Ack")
-                sock.send(POSITIVE_ACK)
-            else:
-                print("Sending Negative Ack")
-                sock.send(NEGATIVE_ACK)
+            if data:
+                data = data.decode("utf-8")
+                print(f"Received: {data}")
+                remainder = decodeData(data, CRC)
+                if int(remainder) == 0:
+                    msg_correctly_received = True
+                if msg_correctly_received:
+                    data = data[:-(len(CRC) - 1)]
+                    received_size += len(data)
+                    print(f"Received this much data so far: {received_size}")
+                    f.write(data.encode("utf-8"))
+                    print("Sending Positive Ack")
+                    sock.send(POSITIVE_ACK)
+                else:
+                    print("Sending Negative Ack")
+                    sock.send(NEGATIVE_ACK)
+                global_counter += 1
+                print(str(global_counter))
+                print("\n")
+    end_time = time.time()
     print("File successfully received complete")
+    through_put = calculate_throughput(file_size, (end_time - start_time))
+    
+
     f.close()
 
+def calculate_throughput(file_size, transfer_time):
+    through_put = file_size * 8 / transfer_time
+    return through_put
+
+
+def check_diff():
+    pass
+
 def Main():
-    s = socket.socket()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.bind((HOST, PORT[0]))
     except Exception as e:
@@ -61,3 +128,4 @@ def Main():
 
 if __name__ == "__main__":
     Main()
+
